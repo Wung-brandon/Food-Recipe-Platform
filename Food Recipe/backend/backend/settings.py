958061172a47e14d1b7a2a78b5bc5ec2bcb9cafd
Wung Brandon <wungbrandon27@gmail.com>
2025-04-24 08:58:51 +0100

    added the recipe app to add, edit and delete recipes for authenticated users

--- Food Recipe/backend/backend/settings.py
+++ Food Recipe/backend/backend/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     "authentication.apps.AuthenticationConfig",
+    "recipe.apps.RecipeConfig",
     
     "rest_framework",
     "corsheaders",
---
+++ Food Recipe/backend/recipe/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # path("recipe/", RecipeList.as_view(), name="recipe-list"),
+    # path("recipe/<int:pk>/", RecipeDetail.as_view(), name="recipe-detail"),
+    # path("recipe/<int:pk>/like/", RecipeLike.as_view(), name="recipe-like"),
+    # path("recipe/<int:pk>/unlike/", RecipeUnlike.as_view(), name="recipe-unlike"),
+    # path("recipe/<int:pk>/comment/", RecipeComment.as_view(), name="recipe-comment"),
+]
